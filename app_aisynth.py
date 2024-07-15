46eb19de59999653de44707be2ace1a06300ab7f
Yew Mun <yewmun.yip@crick.ac.uk>
2024-07-15 11:29:37 +0100

    add functionality to display molecule in 2D

--- app_aisynth.py
+++ app_aisynth.py
@@ -1,27 +1,26 @@
 import streamlit as st
-from aizynthfinder.interfaces import AiZynthApp
+from rdkit import Chem
+from rdkit.Chem import Draw
 
-#app = AiZynthApp("config.yml")
+# from aizynthfinder.interfaces import AiZynthApp
 
+# app = AiZynthApp("config.yml")
 
 
 user_input_smiles = st.text_input("SMILES", "")
 # TODO: Implement visualisation of SMILES
+if user_input_smiles != "":
+    rdkit_molecule = Chem.MolFromSmiles(user_input_smiles)
+    Draw.MolToFile(rdkit_molecule, "images/rdkit_molecule.png")
+    st.image("images/rdkit_molecule.png")
 
 
-
-user_input_stocks = st.selectbox(
-    "Stocks",
-    ("", "zinc"))
+user_input_stocks = st.selectbox("Stocks", ("", "zinc"))
 
 
-
-user_input_exp_pol = st.selectbox(
-    "Expansion Policy",
-    ("uspto", "ringbreaker"))
+user_input_exp_pol = st.selectbox("Expansion Policy", ("uspto", "ringbreaker"))
 
 
-
-#st.write("**Filter Policy**")
+# st.write("**Filter Policy**")
 
 min_time = st.slider("**Time**(minimum)", 0, 100)
